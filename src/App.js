24c1b65ceaba9732dc1aee19cb8b039fc37c2960
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-02-03 11:10:50 +0530

    Simple bar graph created.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React from "react";
 import "./style.less";
 
+import "react-vis/dist/style.css";
+
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  VerticalBarSeries,
+  VerticalBarSeriesCanvas,
+  LabelSeries
+} from "react-vis";
+
 // Component Imports
 import Navbar from "./components/navbar/Navbar.js";
 
-export default function App() {
-  return (
-    <div>
-      <Navbar />
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      useCanvas: false
+    };
+  }
+
+  render() {
+    const greenData = [{ x: "A", y: 10 }, { x: "B", y: 5 }, { x: "C", y: 15 }];
+
+    const blueData = [{ x: "A", y: 12 }, { x: "B", y: 2 }, { x: "C", y: 11 }];
+
+    const labelData = greenData.map((d, idx) => ({
+      x: d.x,
+      y: Math.max(greenData[idx].y, blueData[idx].y)
+    }));
+
+    const { useCanvas } = this.state;
+    const content = useCanvas ? "TOGGLE TO SVG" : "TOGGLE TO CANVAS";
+    const BarSeries = useCanvas ? VerticalBarSeriesCanvas : VerticalBarSeries;
+
+    return (
+      <div>
+        <Navbar />
+        <h1>Mayaskara</h1>
+        <XYPlot xType="ordinal" width={300} height={300} xDistance={100}>
+          <XAxis />
+          <YAxis />
+          <BarSeries className="vertical-bar-series-example" data={greenData} />
+          <BarSeries data={blueData} />
+          <LabelSeries data={labelData} getLabel={d => d.x} />
+        </XYPlot>
+      </div>
+    );
+  }
 }
+
+export default App;
