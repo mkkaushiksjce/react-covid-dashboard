2c1bda0344d797c871fe102f7d2bfd4481fbe497
mkkaushiksjce <mkkaushiksjce@gmail.com>
2021-02-11 06:29:56 +0530

    Horizontal stacked chart component completed

--- src/components/stackedbarchart/StackedBarchart.js
+++ src/components/stackedbarchart/StackedBarchart.js
@@ -9,8 +9,8 @@ import {
   YAxis,
   VerticalGridLines,
   HorizontalGridLines,
-  VerticalBarSeries,
-  VerticalBarSeriesCanvas
+  HorizontalBarSeries,
+  HorizontalBarSeriesCanvas
 } from "react-vis";
 
 class StackedBarchart extends React.Component {
@@ -21,19 +21,34 @@ class StackedBarchart extends React.Component {
     };
   }
 
+  /* 
+  Horizontal 
+    <BarSeries data={[{ x: 0, y: 2 }, { x: 4, y: 5 }, { x: 7, y: 10 }]} />
+    <BarSeries data={[{ x: 0, y: 3 }, { x: 4, y: 5 }, { x: 7, y: 11 }]} />  
+  */
+
   render() {
     const { useCanvas } = this.state;
-    const BarSeries = useCanvas ? VerticalBarSeriesCanvas : VerticalBarSeries;
+    // Option to change from horizontal or vertical
+    // along with the data for horizontal (y, x)
+    // Vertical (x, y)
+    const BarSeries = useCanvas
+      ? HorizontalBarSeriesCanvas
+      : HorizontalBarSeries;
     const content = useCanvas ? "TOGGLE TO SVG" : "TOGGLE TO CANVAS";
     return (
       <div>
-        <XYPlot width={300} height={300} stackBy="y">
+        <XYPlot width={300} height={300} stackBy="x">
           <VerticalGridLines />
           <HorizontalGridLines />
           <XAxis />
           <YAxis />
-          <BarSeries data={[{ x: 0, y: 2 }, { x: 4, y: 5 }, { x: 7, y: 10 }]} />
-          <BarSeries data={[{ x: 0, y: 3 }, { x: 4, y: 5 }, { x: 7, y: 11 }]} />
+          <BarSeries
+            data={[{ y: 2, x: 10 }, { y: 4, x: 5 }, { y: 5, x: 15 }]}
+          />
+          <BarSeries
+            data={[{ y: 2, x: 12 }, { y: 4, x: 2 }, { y: 5, x: 11 }]}
+          />
         </XYPlot>
       </div>
     );
